3d284763ea6018c081bd64dc6e73cc3df8d5b6b3
James Sammut <juma2791@gmail.com>
2023-06-20 17:08:17 +1000

    Cluster Queue create implementation

--- buildkite/resource_cluster_queue.go
+++ buildkite/resource_cluster_queue.go
@@ -14,10 +14,12 @@ type ClusterQueueResource struct {
 }
 
 type ClusterQueueResourceModel struct {
-	ID          types.String `tfsdk:"id"`
-	UUID		types.String `tfsdk:"uuid"`
-	Key	        types.String `tfsdk:"key"`
-	Description types.String `tfsdk:"description"`
+	ID             types.String `tfsdk:"id"`
+	UUID           types.String `tfsdk:"uuid"`
+	OrganizationID types.String `tfsdk:"organizationId"`
+	ClusterID      types.String `tfsdk:"clusterId"`
+	Key            types.String `tfsdk:"key"`
+	Description    types.String `tfsdk:"description"`
 }
 
 func (c *ClusterQueueResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -32,8 +34,16 @@ func (c *ClusterQueueResource) Schema(ctx context.Context, req resource.SchemaRe
 				Computed: true,
 			},
 			"uuid": schema.StringAttribute{
-				Computed: 			 true,
+				Computed:            true,
 				MarkdownDescription: "The public UUID of the Cluster Queue.",
+			},
+			"organizationId": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The ID of the Organization that the Cluster belongs to.",
+			},
+			"clusterId": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The ID of the Cluster that this Cluster Queue belongs to.",
 			},
 			"key": schema.StringAttribute{
 				Required:            true,
@@ -54,6 +64,27 @@ func (c *ClusterQueueResource) Create(ctx context.Context, req resource.CreateRe
 	if resp.Diagnostics.HasError() {
 		return
 	}
+
+	createReq := ClusterQueueCreateInput{
+		OrganizationId: data.OrganizationID.ValueString(),
+		ClusterId:      data.ClusterID.ValueString(),
+		Description:    data.Description.ValueString(),
+		Key:            data.Key.ValueString(),
+	}
+
+	r, err := createClusterQueue(c.client.genqlient, createReq)
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create Cluster Queue",
+			fmt.Sprintf("Unable to create Cluster: %s", err.Error()),
+		)
+		return
+	}
+
+	data.ID = types.StringValue(r.ClusterQueueCreate.ClusterQueue.Id)
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (c *ClusterQueueResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -61,8 +92,8 @@ func (c *ClusterQueueResource) Read(ctx context.Context, req resource.ReadReques
 }
 
 func (c *ClusterQueueResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	
-}	/* ... */
+
+} /* ... */
 
 func (c *ClusterQueueResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	/* ... */
