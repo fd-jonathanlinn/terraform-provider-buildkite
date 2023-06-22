b68a561103fa63279678d7447201d8c9cde374b1
James Sammut <juma2791@gmail.com>
2023-06-22 12:23:30 +1000

    Create, Update, Delete complete for Cluster Queues

--- buildkite/resource_cluster_queue.go
+++ buildkite/resource_cluster_queue.go
@@ -2,31 +2,32 @@ package buildkite
 
 import (
 	"context"
-	"fmt"
+	//"fmt"
 
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 	resource_schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-type ClusterQueueResourceModel struct {
-	ID             types.String `tfsdk:"id"`
-	UUID           types.String `tfsdk:"uuid"`
-	OrganizationID types.String `tfsdk:"organizationId"`
-	ClusterID      types.String `tfsdk:"clusterId"`
+type ClusterQueueStateModel struct {
+	Id             types.String `tfsdk:"id"`
+	Uuid           types.String `tfsdk:"uuid"`
+	OrganizationId types.String `tfsdk:"organization_id"`
+	ClusterId      types.String `tfsdk:"cluster_id"`
 	Key            types.String `tfsdk:"key"`
 	Description    types.String `tfsdk:"description"`
 }
 
-type CluseterQueue struct {
+type CluseterQueueResource struct {
 	client *Client
 }
 
-func NewClusterQueue() resource.Resource {
-	return &CluseterQueue{}
+func NewClusterQueueResource() resource.Resource {
+	return &CluseterQueueResource{}
 }
 
-func (CluseterQueue) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (CluseterQueueResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = resource_schema.Schema{
 		MarkdownDescription: "A Cluster Queue is a queue belonging to a specific Cluster for its Agents to target builds on. ",
 		Attributes: map[string]resource_schema.Attribute{
@@ -37,11 +38,11 @@ func (CluseterQueue) Schema(ctx context.Context, req resource.SchemaRequest, res
 				Computed:            true,
 				MarkdownDescription: "The public UUID of the Cluster Queue.",
 			},
-			"organizationId": resource_schema.StringAttribute{
+			"organization_id": resource_schema.StringAttribute{
 				Required:            true,
 				MarkdownDescription: "The ID of the Organization that the Cluster belongs to.",
 			},
-			"clusterId": resource_schema.StringAttribute{
+			"cluster_id": resource_schema.StringAttribute{
 				Required:            true,
 				MarkdownDescription: "The ID of the Cluster that this Cluster Queue belongs to.",
 			},
@@ -57,55 +58,82 @@ func (CluseterQueue) Schema(ctx context.Context, req resource.SchemaRequest, res
 	}
 }
 
-func (CluseterQueue) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+func (CluseterQueueResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
 	resp.TypeName = "buildkite_cluster_queue"
 }
 
-func (cq *CluseterQueue) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (cq *CluseterQueueResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
 	cq.client = req.ProviderData.(*Client)
 }
 
-func (cq *CluseterQueue) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data *ClusterQueueResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	createReq := ClusterQueueCreateInput{
-		OrganizationId: data.OrganizationID.ValueString(),
-		ClusterId:      data.ClusterID.ValueString(),
-		Description:    data.Description.ValueString(),
-		Key:            data.Key.ValueString(),
-	}
-
-	r, err := createClusterQueue(cq.client.genqlient, createReq)
+func (cq *CluseterQueueResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan, state ClusterQueueStateModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	
+	apiResponse, err := createClusterQueue(
+		cq.client.genqlient,
+		cq.client.organizationId,
+		plan.ClusterId.ValueString(),
+		plan.Key.ValueString(),
+		plan.Description.ValueStringPointer(),
+	)
 
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Unable to create Cluster Queue",
-			fmt.Sprintf("Unable to create Cluster: %s", err.Error()),
-		)
-		return
+		resp.Diagnostics.AddError(err.Error(), err.Error())
 	}
 
-	data.ID = types.StringValue(r.ClusterQueueCreate.ClusterQueue.Id)
+	state.Id = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Id)
+	state.Uuid = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Uuid)
+	state.OrganizationId = types.StringValue(cq.client.organizationId)
+	state.ClusterId = plan.ClusterId
+	state.Key = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Key)
+	state.Description = types.StringPointerValue(&apiResponse.ClusterQueueCreate.ClusterQueue.Description)
 
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
 }
 
-func (cq *CluseterQueue) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	/* ... */
+func (cq *CluseterQueueResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// To implement
 }
 
-func (cq *CluseterQueue) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+func (cq *CluseterQueueResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var state ClusterQueueStateModel
+	var description string
 
-} /* ... */
+	//Load state and ontain description from plan (singularly)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, path.Root("description"), &description)...)
 
-func (cq *CluseterQueue) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	/* ... */
+	apiResponse, err := updateClusterQueue(
+		cq.client.genqlient,
+		cq.client.organizationId,
+		state.Id.ValueString(),
+		&description,
+	)
+
+	if err != nil {
+		resp.Diagnostics.AddError(err.Error(), err.Error())
+	}
+
+	state.Description = types.StringPointerValue(&apiResponse.ClusterQueueUpdate.ClusterQueue.Description)
+	
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (cq *CluseterQueueResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var plan ClusterQueueStateModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &plan)...)
+
+	_, err := deleteClusterQueue(
+		cq.client.genqlient,
+		cq.client.organizationId,
+		plan.Id.ValueString(),
+	)
+
+	if err != nil {
+		resp.Diagnostics.AddError(err.Error(), err.Error())
+	}
 }
