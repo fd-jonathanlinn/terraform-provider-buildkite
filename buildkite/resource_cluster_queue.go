985265d501f63e6cbd53fee83cb1fff4221ce594
James Sammut <juma2791@gmail.com>
2023-06-29 15:59:47 +1000

    GraphQL fragments, updated Read function, test dev

--- buildkite/resource_cluster_queue.go
+++ buildkite/resource_cluster_queue.go
@@ -12,10 +12,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-type ClusterQueueStateModel struct {
+type ClusterQueueResourceModel struct {
 	Id             types.String `tfsdk:"id"`
 	Uuid           types.String `tfsdk:"uuid"`
 	ClusterId      types.String `tfsdk:"cluster_id"`
+	ClusterUuid    types.String `tfsdk:"cluster_uuid"`
 	Key            types.String `tfsdk:"key"`
 	Description    types.String `tfsdk:"description"`
 }
@@ -26,6 +27,18 @@ type ClusterQueueResource struct {
 
 func NewClusterQueueResource() resource.Resource {
 	return &ClusterQueueResource{}
+}
+
+func (ClusterQueueResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_cluster_queue"
+}
+
+func (cq *ClusterQueueResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	cq.client = req.ProviderData.(*Client)
 }
 
 func (ClusterQueueResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
@@ -39,6 +52,12 @@ func (ClusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 				},
 			},
 			"uuid": resource_schema.StringAttribute{
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"cluster_uuid": resource_schema.StringAttribute{
 				Computed:            true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
@@ -60,20 +79,14 @@ func (ClusterQueueResource) Schema(ctx context.Context, req resource.SchemaReque
 	}
 }
 
-func (ClusterQueueResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = "buildkite_cluster_queue"
-}
+func (cq *ClusterQueueResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan, state ClusterQueueResourceModel
 
-func (cq *ClusterQueueResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
-	if req.ProviderData == nil {
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+
+	if resp.Diagnostics.HasError() {
 		return
 	}
-	cq.client = req.ProviderData.(*Client)
-}
-
-func (cq *ClusterQueueResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var plan, state ClusterQueueStateModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 
 	apiResponse, err := createClusterQueue(
 		cq.client.genqlient,
@@ -94,6 +107,7 @@ func (cq *ClusterQueueResource) Create(ctx context.Context, req resource.CreateR
 	state.Id = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Id)
 	state.Uuid = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Uuid)
 	state.ClusterId = plan.ClusterId
+	state.ClusterUuid = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Cluster.Uuid)
 	state.Key = types.StringValue(apiResponse.ClusterQueueCreate.ClusterQueue.Key)
 	state.Description = types.StringPointerValue(&apiResponse.ClusterQueueCreate.ClusterQueue.Description)
 
@@ -101,16 +115,46 @@ func (cq *ClusterQueueResource) Create(ctx context.Context, req resource.CreateR
 }
 
 func (cq *ClusterQueueResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	// To implement
+	var state ClusterQueueResourceModel
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	queues, err := getClusterQueues(cq.client.genqlient, cq.client.organization, state.ClusterUuid.ValueString())
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to read Cluster Queue",
+			fmt.Sprintf("Unable to read Cluster Queue: %s", err.Error()),
+		)
+		return
+	}
+
+	// Find the Cluster Q from the returned queues to update state
+	for i := range queues.Organization.Cluster.Queues.Edges {
+		if queues.Organization.Cluster.Queues.Edges[i].Node.Id == state.Id.ValueString(){
+			// Update ClusterQueueResourceModel with Node values and append
+			updateClusterQueueResource(queues.Organization.Cluster.Queues.Edges[i].Node, &state)
+			resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+			break
+		}
+	}
 }
 
 func (cq *ClusterQueueResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var state ClusterQueueStateModel
+	var state ClusterQueueResourceModel
 	var description string
 
 	//Load state and ontain description from plan (singularly)
 	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
 	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, path.Root("description"), &description)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
 	apiResponse, err := updateClusterQueue(
 		cq.client.genqlient,
@@ -133,8 +177,13 @@ func (cq *ClusterQueueResource) Update(ctx context.Context, req resource.UpdateR
 }
 
 func (cq *ClusterQueueResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var plan ClusterQueueStateModel
+	var plan ClusterQueueResourceModel
+
 	resp.Diagnostics.Append(req.State.Get(ctx, &plan)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
 	_, err := deleteClusterQueue(
 		cq.client.genqlient,
@@ -149,4 +198,13 @@ func (cq *ClusterQueueResource) Delete(ctx context.Context, req resource.DeleteR
 		)
 		return
 	}
+}
+
+func updateClusterQueueResource(clusterQueueNode getClusterQueuesOrganizationClusterQueuesClusterQueueConnectionEdgesClusterQueueEdgeNodeClusterQueue, cq *ClusterQueueResourceModel){
+	cq.Id = types.StringValue(clusterQueueNode.Id)
+	cq.Uuid = types.StringValue(clusterQueueNode.Uuid)
+	cq.Key = types.StringValue(clusterQueueNode.Key)
+	cq.Description = types.StringPointerValue(&clusterQueueNode.Description)
+	cq.ClusterId = types.StringValue(clusterQueueNode.Cluster.Id)
+	cq.ClusterUuid = types.StringValue(clusterQueueNode.Cluster.Uuid)
 }
